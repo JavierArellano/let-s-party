6b429d4aa7604676ed0ed9829435a2c19b121695
Javier Arellano <franjarca@hotmail.com>
2018-06-20 19:17:36 +0200

    imagen de perfil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Camera } from '@ionic-native/camera';
 
 import { MyApp } from './app.component';
 import {
@@ -85,10 +86,11 @@ import { UsuarioProvider } from '../providers/usuario/usuario';
     AuthSProvider,
     PartiesProvider,
     UbicacionProvider,
+    UsuarioProvider,
     Geolocation,
     SocialSharing,
     Calendar,
-    UsuarioProvider
+    Camera
   ]
 })
 export class AppModule {}
